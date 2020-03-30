12ac36cb9530e6203a547fe15162b42f20627044
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-30 00:42:45 -0400

    exports two new hooks

--- game/hooks/index.js
+++ game/hooks/index.js
@@ -1,2 +1,4 @@
+export { default as useTimer } from './useTimer';
+export { default as useFormatSeconds } from './useFormatSeconds';
 export { default as useInterval } from './useInterval';
 export { default as useListenKeyPress } from './useListenKeyPress';
