58ca2180ab3022f11c75d4919678955807b2c5d1
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-30 00:43:50 -0400

    updates game reducer

--- store/reducers/gameReducer.js
+++ store/reducers/gameReducer.js
@@ -1,6 +1,13 @@
 import { G } from '../../game/constants';
-import { RESTART_GAME } from '../actions/gameActions';
-import { CALCULATE_SCORE, GAME_OVER, START_GAME } from '../index';
+import {
+  CALCULATE_SCORE,
+  GAME_OVER,
+  INCREMENT_ATTACK,
+  INCREMENT_TETRADS_PROCESSED,
+  INCREMENT_TIME,
+  RESTART_GAME,
+  START_GAME
+} from '../index';
 const initialState = {
   gameStarted: false,
   gameOver: false,
@@ -11,8 +18,20 @@ const initialState = {
   topScore: 0,
   tetrisNum: 0,
   tetrisRate: 0,
-  apm: 0,
-  ppm: 0
+  attacks: 0,
+  aps: 0, // attacks per second
+  tetradsProcessed: -1,
+  tps: 0, // tetrads processed per second
+  totalSeconds: 0,
+  tetradCount: {
+    T: 0,
+    Z: 0,
+    S: 0,
+    I: 0,
+    J: 0,
+    O: 0,
+    L: 0
+  }
 };
 
 const gameReducer = (state = initialState, action) => {
@@ -21,6 +40,25 @@ const gameReducer = (state = initialState, action) => {
       return {
         ...state,
         gameStarted: true
+      };
+    case INCREMENT_TIME:
+      return {
+        ...state,
+        totalSeconds: action.payload.totalSeconds,
+        aps: action.payload.aps,
+        tps: action.payload.tps
+      };
+
+    case INCREMENT_ATTACK:
+      return {
+        ...state,
+        attacks: ++state.attacks
+      };
+    case INCREMENT_TETRADS_PROCESSED:
+      return {
+        ...state,
+        tetradsProcessed: ++state.tetradsProcessed,
+        tetradCount: action.payload.tetradCount
       };
     case GAME_OVER: {
       return {
