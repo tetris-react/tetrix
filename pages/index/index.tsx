0a3d18fd1beb02467cfc54b2bff3a49b526acd3d
Dave Vazquez <dmvaz.li@gmail.com>
2020-04-02 10:07:15 -0400

    creates landing-page component

--- pages/index/index.tsx
+++ pages/index/index.tsx
@@ -1,16 +1,11 @@
-import Playfield from '../../game/components/playfield/Playfield';
-import ScoreBoard from '../../game/components/score/ScoreBoard';
-import Statistics from '../../game/components/stats/Statistics';
-import { AppContainer, ButtonDialog } from '../../game/styles';
-function Index() {
+import { AppContainer } from '../../game/styles';
+
+const Index = () => {
   return (
     <AppContainer>
-      <Statistics />
-      <Playfield />
-      <ScoreBoard />
-      <ButtonDialog />
+      <h1>Tetrix</h1>
     </AppContainer>
   );
-}
+};
 
 export default Index;
