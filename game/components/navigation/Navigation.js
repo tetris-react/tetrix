b8e71ae9d37f0912d489b92f89ff4065426b9d40
Dave Vazquez <dmvaz.li@gmail.com>
2020-04-02 10:44:10 -0400

    creates Navigation component

---
+++ game/components/navigation/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { NavContainer } from '../../styles';
+
+const Navigation = () => {
+  return (
+    <NavContainer>
+      <div />
+    </NavContainer>
+  );
+};
+
+export default Navigation;
