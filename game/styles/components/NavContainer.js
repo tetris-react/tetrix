7619083d463a8e7b6c0f2cb5cc78d8ac33fc82dd
Dave Vazquez <dmvaz.li@gmail.com>
2020-04-02 10:43:57 -0400

    creates styled-component for Navigation

---
+++ game/styles/components/NavContainer.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const NavContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 5vh;
+  min-height: 5vh;
+
+  border: 1px solid #cccccc;
+`;
