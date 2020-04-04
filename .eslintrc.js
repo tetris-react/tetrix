c61a238ae475730d6cfe0863848e9a78c3ec59e5
JimmyMcBride <mcbride967@gmail.com>
2020-04-04 11:22:11 -0500

    added suttle changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,17 +2,17 @@ module.exports = {
   env: {
     browser: true,
     es6: true,
-    mode: true
+    mode: true,
   },
-  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
   extends: [
-    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
-    'plugin:@typescript-eslint/recommended' // Uses the recommended rules from @typescript-eslint/eslint-plugin
+    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from @typescript-eslint/eslint-plugin
   ],
   parserOptions: {
     ecmaFeatures: {
-      jsx: true // Allows for the parsing of JSX
-    }
+      jsx: true, // Allows for the parsing of JSX
+    },
   },
   rules: {
     // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -20,16 +20,17 @@ module.exports = {
   },
   settings: {
     react: {
-      version: 'detect' // Tells eslint-plugin-react to automatically detect the version of React to use
-    }
+      version: "detect", // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
   },
-  plugins: ['react'],
+  plugins: ["react", "prettier"],
+  extends: ["prettier"],
   overrides: [
     {
-      files: ['**/*.test.js'],
+      files: ["**/*.test.js"],
       env: {
-        jest: true
-      }
-    }
-  ]
+        jest: true,
+      },
+    },
+  ],
 };
